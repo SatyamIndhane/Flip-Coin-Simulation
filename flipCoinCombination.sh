98f4e3fb9fb5aa662aee118a6f2bc7514dd74c0f
SatyamIndhane <satyam.indhane@gmail.com>
2021-01-14 19:26:01 +0530

    Triplet Combination with their percentage

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -9,52 +9,90 @@ declare -A coin
 
 for (( counter=1; counter<=n; counter++ ))
 do
-	var=$((RANDOM%4))
+	var=$((RANDOM%8))
 	if (( var == 0 ))
 	then
-		coin[$counter]="HH"
+		coin[$counter]="HHH"
 	elif (( var == 1 ))
 	then
-		coin[$counter]="TT"
+		coin[$counter]="TTT"
 	elif (( var == 2 ))
 	then
-		coin[$counter]="HT"
-	else
-		coin[$counter]="TT"
+		coin[$counter]="HTT"
+	elif (( var == 3 ))
+	then
+		coin[$counter]="TTH"
+	elif (( var == 4 ))
+	then
+		coin[$counter]="HHT"
+	elif (( var == 5 ))
+	then
+		coin[$counter]="THH"
+	elif (( var == 6 ))
+	then
+		coin[$counter]="HTH"
+	elif (( var == 7 ))
+	then
+		coin[$counter]="THT"
 	fi
 done
 
 echo ${!coin[@]} ${coin[@]}
 
-hh=0
-tt=0
-ht=0
-th=0
+hhh=0
+ttt=0
+htt=0
+tth=0
+hht=0
+thh=0
+hth=0
+tht=0
 
 for i in ${coin[@]}
 do
-	if [ "$i" == "HH" ]
+	if [ "$i" == "HHH" ]
 	then
-		((hh++))
-	elif [ "$i" == "TT" ]
+		((hhh++))
+	elif [ "$i" == "TTT" ]
 	then
-		((tt++))
-	elif [ "$i" == "ht" ]
+		((ttt++))
+	elif [ "$i" == "HTT" ]
 	then
-		((ht++))
-	else
-		((th++))
+		((htt++))
+	elif [ "$i" == "TTH" ]
+	then
+		((tth++))
+	elif [ "$i" == "HHT" ]
+	then
+		((hht++))
+	elif [ "$i" == "THH" ]
+	then
+		((thh++))
+	elif [ "$i" == "HTH" ]
+	then
+		((hth++))
+	elif [ "$i" == "THT" ]
+	then
+		((tht++))
 	fi
 done
 
-echo $hh $tt $ht $th
+echo $hhh $ttt $htt $tth $hht $thh $hth $tht
 
-Phh=$(( (hh*100)/n ))
-Ptt=$(( (tt*100)/n ))
-Pht=$(( (ht*100)/n ))
-Pth=$(( (th*100)/n ))
+Phhh=$(( (hhh*100)/n ))
+Pttt=$(( (ttt*100)/n ))
+Phtt=$(( (htt*100)/n ))
+Ptth=$(( (tth*100)/n ))
+Phht=$(( (hht*100)/n ))
+Pthh=$(( (thh*100)/n ))
+Phth=$(( (hth*100)/n ))
+Ptht=$(( (tht*100)/n ))
 
-echo Doublet Percentage of HH is $Phh
-echo Doublet Percentage of TT is $Ptt
-echo Doublet Percentage of HT is $Pht
-echo Doublet Percentage of TH is $Pth
+echo Doublet Percentage of HHH is $Phhh
+echo Doublet Percentage of TTT is $Pttt
+echo Doublet Percentage of HTT is $Phtt
+echo Doublet Percentage of TTH is $Ptth
+echo Doublet Percentage of HHT is $Phht
+echo Doublet Percentage of THH is $Pthh
+echo Doublet Percentage of HTH is $Phth
+echo Doublet Percentage of THT is $Ptht

8ecc8ed74dfe91bc192293595e9fb1e505dd3e39
SatyamIndhane <satyam.indhane@gmail.com>
2021-01-14 18:49:13 +0530

    flipping coin multiple times and storing data in dictionary with percentage of singlet combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,11 +2,42 @@
 
 #Welcome to flip coin simulation problem
 
-var=$((RANDOM%2))
+read -p "Enter the times to flip the coin:" n;
 
-if (( var == 1 ))
-then
-	echo Heads
-else
-	echo Tails
-fi
+
+declare -A coin
+
+for (( counter=1; counter<=n; counter++ ))
+do
+	var=$((RANDOM%2))
+	if (( var == 1 ))
+	then
+		coin[$counter]="H"
+	else
+		coin[$counter]="T"
+	fi
+done
+
+echo ${!coin[@]} ${coin[@]}
+
+heads=0
+tails=0
+
+for i in ${coin[@]}
+do
+	if [ "$i" == "H" ]
+	then
+		((heads++))
+	else
+		((tails++))
+	fi
+done
+
+echo $heads $tails
+
+PH=$(( (heads*100)/n ))
+PT=$(( (tails*100)/n ))
+
+echo Singlet Percentage of heads is $PH
+echo Singlet Percentage of tails is $PT
+
